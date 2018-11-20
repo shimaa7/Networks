4f2fea4ab163b7687864b90d6d81c950c99dbfb3
Shimaa Hassan <eng.shimaa71296@gmail.com>
2018-11-20 19:12:09 +0200

    alter function implementation

--- CRC algorithm/main.cpp
+++ CRC algorithm/main.cpp
@@ -55,7 +55,11 @@ bool verifier(string codeword,string key){
 
 }
 string alter(string data, int index){
-
+    string dataModified = data;
+    char str = data.at(index - 1); // input start from 1 but we start from 0
+    str = (str == '0')?'1':'0';
+    dataModified.replace(dataModified.begin() + index,1,string(str));
+    return dataModified;
 }
 
 int main()
