4839179689e70b21bc28c728cc7c970dcf943e4a
Menna Allah Muhammed Abd el rahman <menna.muhammed198@gmail.com>
2018-11-20 18:59:48 +0200

    mod function

--- CRC algorithm/main.cpp
+++ CRC algorithm/main.cpp
@@ -9,7 +9,29 @@ string xorFn(string a,string b){
 }
 string mod2div(string key, string data)
 {
+    int nBits = key.length();
+    string temp = data.substr(0,nBits);
+    string zDiv ="";
+    while(nBits<data.length()){
+        if(temp[0] == '1'){
+            temp = xorFn(temp,key)+data[nBits];
+        }
+        else{
+            zDiv = string(nBits,'0');
+            temp = xorFn(temp,zDiv)+data[nBits];
+        }
+        nBits++;
+    }
+    if(temp[0]=='1'){
+        temp = xorFn(temp,key);
 
+    }
+    else{
+        zDiv = string(nBits,'0');
+        temp = xorFn(temp,zDiv);
+    }
+
+    return temp;
 }
 string encodeData(string data,string key)
 {
